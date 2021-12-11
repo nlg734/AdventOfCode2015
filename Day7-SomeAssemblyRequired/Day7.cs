289547ce1425ed66b034e043971b438bebe66a5b
Natasha Graham <nlgraham95@gmail.com>
2021-12-10 16:38:48 -0900

    Day 7 complete

--- Day7-SomeAssemblyRequired/Day7.cs
+++ Day7-SomeAssemblyRequired/Day7.cs
@@ -1,105 +1,116 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-string input = Console.ReadLine();
-var wires = new Dictionary<string, UInt16>();
+string? input = Console.ReadLine();
+var wires = new Dictionary<string, string>();  // wires and their equivalent expressions
+var values = new Dictionary<string, UInt16>();  // wires and their numerical values
+
+
+/// <summary>
+/// Takes input, puts into wires
+/// </summary>
 while (!String.IsNullOrEmpty(input))
 {
-    if (input.Contains("AND"))
-    {
-        string[] values = input.Split(' ');
-        string out_wire = values[4];
-        bool l = wires.TryGetValue(values[0], out UInt16 left);
-        bool r = wires.TryGetValue(values[2], out UInt16 right);
-
-        if (!l)
-        {
-            left = UInt16.Parse(values[0]);
-        }
-        if (!r)
-        {
-            right = UInt16.Parse(values[2]);
-        }
-
-        wires[out_wire] = (UInt16)(left & right);
-    }
-    else if (input.Contains("OR"))
-    {
-        string[] values = input.Split(' ');
-        string out_wire = values[4];
-        bool l = wires.TryGetValue(values[0], out UInt16 left);
-        bool r = wires.TryGetValue(values[2], out UInt16 right);
-
-        if (!l)
-        {
-            left = UInt16.Parse(values[0]);
-        }
-        if (!r)
-        {
-            right = UInt16.Parse(values[2]);
-        }
-
-        wires[out_wire] = (UInt16)(left | right);
-    }
-    else if (input.Contains("LSHIFT"))
-    {
-        string[] values = input.Split(' ');
-        string out_wire = values[4];
-        bool l = wires.TryGetValue(values[0], out UInt16 left);
-        bool r = wires.TryGetValue(values[2], out UInt16 right);
-
-        if (!l)
-        {
-            left = UInt16.Parse(values[0]);
-        }
-        if (!r)
-        {
-            right = UInt16.Parse(values[2]);
-        }
-
-        wires[out_wire] = (UInt16)(left << right);
-    }
-    else if (input.Contains("RSHIFT"))
-    {
-        string[] values = input.Split(' ');
-        string out_wire = values[4];
-        bool l = wires.TryGetValue(values[0], out UInt16 left);
-        bool r = wires.TryGetValue(values[2], out UInt16 right);
-
-        if (!l)
-        {
-            left = UInt16.Parse(values[0]);
-        }
-        if (!r)
-        {
-            right = UInt16.Parse(values[2]);
-        }
-
-        wires[out_wire] = (UInt16)(left >> right);
-    }
-    else if (input.Contains("NOT"))
-    {
-        string[] values = input.Split(' ');
-        string out_wire = values[3];
-        bool r = wires.TryGetValue(values[1], out UInt16 right);
-
-        if (!r)
-        {
-            right = UInt16.Parse(values[1]);
-        }
-
-        wires[out_wire] = (UInt16)~right;
-    }
-    else
-    {
-        string[] values = input.Split(" -> ");
-        // wires.TryGetValue(values[1], out Int16 num);
-        wires[values[1]] = UInt16.Parse(values[0]);
-    }
+    string[] eq = input.Split(" -> ");
+    wires[eq[1]] = eq[0];
 
     input = Console.ReadLine();
 }
 
+values["b"] = 16076;  // For part 2
+
+/// <summary>
+/// fill in values for each key
+/// </summary>
 foreach(string key in wires.Keys)
 {
-    Console.WriteLine(key + ": " + wires[key]);
+    // For part 2
+    if(key == "b")
+    {
+        continue;
+    }
+    values[key] = GetValue(key);
+}
+
+
+/// <summary>
+/// Print out wires and values
+/// </summary>
+foreach(string key in values.Keys)
+{
+    Console.WriteLine(key + ": " + values[key]);
+}
+
+
+/// <summary>
+/// 
+/// </summary>
+UInt16 GetValue(string key)
+{
+    if(values.ContainsKey(key))
+    {
+        return values[key];
+    }
+    if(!wires.ContainsKey(key))
+    {
+        return UInt16.Parse(key);
+    }
+    if(wires[key].Contains("AND"))
+    {
+        string[] variables = wires[key].Split(" AND ");
+        values[key] = AND(GetValue(variables[0]), GetValue(variables[1]));
+        return values[key];
+    }
+    if (wires[key].Contains("OR"))
+    {
+        string[] variables = wires[key].Split(" OR ");
+        values[key] =  OR(GetValue(variables[0]), GetValue(variables[1]));
+        return values[key];
+    }
+    if (wires[key].Contains("NOT"))
+    {
+        string[] variables = wires[key].Split("NOT ");
+        values[key] =  NOT(GetValue(variables[1]));
+        return values[key];
+    }
+    if (wires[key].Contains("RSHIFT"))
+    {
+        string[] variables = wires[key].Split(" RSHIFT ");
+        values[key] = RSHIFT(GetValue(variables[0]), GetValue(variables[1]));
+        return values[key];
+    }
+    if (wires[key].Contains("LSHIFT"))
+    {
+        string[] variables = wires[key].Split(" LSHIFT ");
+        values[key] = LSHIFT(GetValue(variables[0]), GetValue(variables[1]));
+        return values[key];
+    }
+
+    values[key] = GetValue(wires[key]);
+    return values[key];
+}
+
+
+UInt16 NOT(UInt16 x)
+{
+    return (UInt16)~x;
+}
+
+UInt16 AND(UInt16 x, UInt16 y)
+{
+    return (UInt16)(x & y);
+}
+
+UInt16 OR(UInt16 x, UInt16 y)
+{
+    return (UInt16)(x | y);
+}
+
+UInt16 RSHIFT(UInt16 x, UInt16 y)
+{
+    return (UInt16)(x >> y);
+}
+
+UInt16 LSHIFT(UInt16 x, UInt16 y)
+{
+    return (UInt16)(x << y);
 }
