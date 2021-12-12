5d48085dba786a28bc2f3cf6f528845bee3fa5a6
Natasha Graham <nlgraham95@gmail.com>
2021-12-12 12:22:00 -0900

    Potential day 10 pt 1 completion. Created project for day 9.

---
+++ Day10-ElvesLookElvesSay/Day10.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+
+string? input = Console.ReadLine();
+
+char current;
+char prev = input[0];
+int count = 1;
+
+for (int j = 0; j < 40; j++)
+{
+    StringBuilder new_str = new StringBuilder();
+    for (int i = 1; i < input.Length; i++)
+    {
+        current = input[i];
+        if (current == prev)
+        {
+            count++;
+        }
+        else
+        {
+            new_str.Append(count.ToString() + prev);
+            prev = current;
+            count = 1;
+        }
+    }
+
+    new_str.Append(count.ToString() + prev);
+    input = new_str.ToString();
+}
+
+Console.WriteLine(input.Length);
